0e025e4783b02a7a429c0caec1148d6d279d6df9
Will Ricketts <will@willricketts.com>
2015-03-19 12:50:52 -0400

    Whitespace cleanup

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express'),
     exphbs  = require('express3-handlebars'),
-
-app = express();
+    app = express();
 
 app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', 'hbs');
@@ -9,18 +8,18 @@ app.set('view engine', 'hbs');
 var Twit = require('twit');
 
 var T = new Twit({
-	consumer_key:         '',
-	consumer_secret:      '',
-	access_token:         '',
-	access_token_secret:  ''
+  consumer_key:        '',
+  consumer_secret:     '',
+  access_token:        '',
+  access_token_secret: ''
 });
 
 app.get('/', function (req, res) {
-	var user = 'GenBurnside';
-	T.get('statuses/user_timeline', { screen_name: user, count: 20 }, function (err, data, response) {
-		console.log('Fetched ' + data.length + ' tweets for ' + user);
-		res.render('home', { tweets : data });
-	});
+  var user = 'GenBurnside';
+  T.get('statuses/user_timeline', { screen_name: user, count: 20 }, function (err, data, response) {
+    console.log('Fetched ' + data.length + ' tweets for ' + user);
+    res.render('home', { tweets : data });
+  });
 });
 
 app.listen(3000);
